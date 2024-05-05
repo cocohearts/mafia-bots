3904ce1cd2db01189ee8ec67ba9bbde64e3f8e84
William Y. Feng <womogenes2@gmail.com>
2024-05-05 11:05:48 -0700

    Make class sorting actuall work

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <h1>Welcome to SvelteKit</h1>
-<p>
+<p class="w-full hover:text-red-500">
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
+<div class="scale-125 opacity-50 hover:scale-150 hover:opacity-75" />
